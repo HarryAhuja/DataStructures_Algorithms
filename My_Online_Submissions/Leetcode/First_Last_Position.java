fa7a9d7701d486b279e4cd0fbb69f2cfc02a644e
harry.ahuja <harryahuja2@gmail.com>
2021-07-27 19:24:12 +0530

    First and Last Index Using Recursion [Binary Search]

--- My_Online_Submissions/Leetcode/First_Last_Position.java
+++ My_Online_Submissions/Leetcode/First_Last_Position.java
@@ -1,61 +1,64 @@
-package datastructures.DataStructures_Algorithms.Library_Api;
+package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 public class First_Last_Position {
+	static private int first_index = -1;
+	static private int last_index  = -1;
 	
-	static public int searchRangeFirstIndex(int[] inp, int target,int low,int high)
+	static public void searchRangeFirstIndex(int[] inp, int target,int low,int high)
 	{
-        int result_index = -1; 
-		while(low<=high)
-		{
-			int mid = low + (high-low)/2;
+        if(low<=high)
+        {
+        	int mid = low + (high-low)/2;
 			
 			if(inp[mid] == target)
 			{
-				result_index = mid;
-				high		 = mid-1;
+				first_index  = mid;
+				searchRangeFirstIndex(inp, target,low,mid-1);
 			}
 			else if(inp[mid] < target)
 			{
-				low = mid+1;
+				searchRangeFirstIndex(inp, target,mid+1,high);
 			}
 			else
 			{
-				high = mid-1;
+				searchRangeFirstIndex(inp, target,low,mid-1);
 			}
-		}
-		return result_index;
-		
+        }
     }
 	
-	static public int searchRangeLastIndex(int[] inp, int target,int low,int high)
+	static public void searchRangeLastIndex(int[] inp, int target,int low,int high)
 	{
-		int result_index = -1; 
-		while(low<=high)
-		{
-			int mid = low + (high-low)/2;
+		if(low<=high)
+        {
+        	int mid = low + (high-low)/2;
 			
 			if(inp[mid] == target)
 			{
-				result_index = mid;
-				low		     = mid+1;
+				last_index  = mid;
+				searchRangeLastIndex(inp, target,mid+1,high);
 			}
 			else if(inp[mid] < target)
 			{
-				low = mid+1;
+				searchRangeLastIndex(inp, target,mid+1,high);
 			}
 			else
 			{
-				high = mid-1;
+				searchRangeLastIndex(inp, target,low,mid-1);
 			}
-		}
-		return result_index;
+        }
 		
     }
 	
-	static public int[] searchRange(int[] nums, int target) {
+	static public int[] searchRange(int[] nums, int target)
+	{
 		int result[] = new int[2];
-		result[0] = searchRangeFirstIndex(nums, target,0,nums.length-1);
-		result[1] = searchRangeLastIndex(nums, target,0,nums.length-1);
+		
+		searchRangeFirstIndex(nums, target,0,nums.length-1);
+		result[0] = first_index;
+		 
+		searchRangeLastIndex(nums, target,0,nums.length-1);
+		result[1] = last_index;
+				
 
 		return result;
     }
