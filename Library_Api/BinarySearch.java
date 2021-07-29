a6f25976f45cdc8168e65979dd118ec2c40886f9
harry.ahuja <harryahuja2@gmail.com>
2021-07-29 19:49:00 +0530

    Find element greater or equal than x [Binary Search library api]

--- Library_Api/BinarySearch.java
+++ Library_Api/BinarySearch.java
@@ -72,4 +72,38 @@ public class BinarySearch {
 		
 		return -1;
     }
+	
+	public int search_greater_equal_element_index(int[] inp, int target, int low, int high)
+    {
+        // Base Cases, if not there then runtime exception in case target>inp[high]
+        // it was returning -1
+        
+        if(target<= inp[0])     return 0;
+        if(target>=inp[high])   return high;
+        
+        
+        int result = -1;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            
+            if(target<inp[mid])
+            {
+                result = mid;
+                high   = mid-1;
+            }
+            else if(target>inp[mid])
+            {
+                low = mid+1;
+            }
+            else
+            {
+                result = mid;
+                break;
+            }
+            
+        }
+        
+        return result;
+    }
 }
