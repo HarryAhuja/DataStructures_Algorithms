cb972d35cf99d3491089b03a50c5cfb93e1c7016
harry.ahuja <harryahuja2@gmail.com>
2021-07-29 21:53:22 +0530

    K Closest Elements using 2 pointers [July Challenge Leetcode]

--- My_Online_Submissions/Leetcode/KClosestElements.java
+++ My_Online_Submissions/Leetcode/KClosestElements.java
@@ -1,93 +1,43 @@
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ListIterator;
 
 public class KClosestElements {
     
-    static int search_greater_equal_element_index_helper(
-    int[] inp, int target, int low, int high, int last_seen)
-    {
-        if(low>high)    return last_seen;
-        
-        int mid = low+(high-low)/2;
-        
-        if(target<inp[mid])
-        {
-            last_seen = mid;
-            return search_greater_equal_element_index_helper(inp,target,low,mid-1,last_seen);
-        }
-        else if(target>inp[mid])
-        {
-            return search_greater_equal_element_index_helper(inp,target,mid+1,high,last_seen);
-        }
-        else
-        {
-            return mid;
-        }
-    }
-    
-    static int search_greater_equal_element_index(int[] inp, int target, int low, int high)
-    {
-        // Base Cases, if not there then runtime exception in case target>inp[high]
-        // it was returning -1
-        
-        if(target<= inp[0])     return 0;
-        if(target>=inp[high])   return high;
-        
-        return search_greater_equal_element_index_helper(inp,target,low,high,-1);
-    }
-
 	public static List<Integer> findClosestElements(int inp[], int k, int x)
 	{
-		List<Integer> result = new ArrayList<Integer>();
-	
-		int n = inp.length;
-		
-		int index = search_greater_equal_element_index(inp, x, 0, n-1);
-		System.out.println(index);
-
-		int count = 0;
-		int i 	  = index-1;
-		int j     = index;
-		
-		while(i>=0 && j<n && count<k)
-		{ 
-		    // Here x should be there, not inp[index] element might not be present
-		    
-			int left_abs_diff = Math.abs(inp[i]-x);          
-			int right_abs_diff = Math.abs(inp[j]-x);         
-			
-			if(left_abs_diff<=right_abs_diff)
-			{
-				result.add(inp[i]);
-				i--;
-			}
-			else if(left_abs_diff>right_abs_diff)
-			{
-				result.add(inp[j]);
-				j++;
-			}
-			count++;
-		}
-		
-		while(i>=0 && count<k)
-		{
-		    result.add(inp[i]);
-		    i--;
-		    count++;
-		}
-		
-		while(j<n && count<k)
-        {
-            result.add(inp[j]);
-            j++;
-            count++;
-            
-        }
-		
-		return result;
+	    int low = 0, high = inp.length-1;
+	    
+	    while(high-low+1>k)
+	    {
+	        int diff_low = Math.abs(inp[low]-x);
+	        int diff_high = Math.abs(inp[high]-x);
+	        
+	        if(diff_low<diff_high)
+	        {
+	            high--;
+	        }
+	        else if(diff_low>diff_high)
+            {
+                low++;
+            }
+	        else
+	        {
+	            high--;
+	        }
+	    }
+	    
+	    List<Integer> result = new ArrayList<Integer>();
+	    
+	    for(int i=low;i<=high;i++)
+	        result.add(inp[i]);
+	    
+	    return result;
+	        
 	}
 	public static void main(String[] args) {
 		
@@ -101,9 +51,7 @@ public class KClosestElements {
 	    int k =2, x=15;
 
 		List<Integer> result = findClosestElements(arr,k,x);
-		result.sort(null);
-		
-		
+			
 		ListIterator<Integer> iter = result.listIterator();
 		
 		while(iter.hasNext())
