eb918a0e330f5458829f45aef0cab307aa2f7cb2
harry.ahuja <harryahuja2@gmail.com>
2021-07-30 19:45:20 +0530

    Search in 2d matrix [Row optimisation]

--- My_Online_Submissions/Leetcode/Search2dMatrix.java
+++ My_Online_Submissions/Leetcode/Search2dMatrix.java
@@ -3,6 +3,60 @@ package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 import datastructures.DataStructures_Algorithms.Library_Api.BinarySearch;
 
 public class Search2dMatrix {
+    
+    public static int find_lower_bound(int[][] matrix, int target, int low, int high)
+    {
+        int result = -1;
+        int cols   = matrix[0].length;
+        
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            
+            if(target>matrix[mid][cols-1])
+            {
+                low = mid+1;
+            }
+            else if(target<matrix[mid][cols-1])
+            {
+                result = mid;
+                high   = mid-1;
+            }
+            else
+            {
+                result = mid;
+                break;
+            }
+        }
+        
+        return result;
+    }
+    
+    public static int find_upper_bound(int[][] matrix, int target, int low, int high)
+    {
+        int result = -1;
+        
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(target>matrix[mid][0])
+            {
+                result = mid;
+                low    = mid+1;
+            }
+            else if(target<matrix[mid][0])
+            {
+                high   = mid-1;
+            }
+            else
+            {
+                result = mid;
+                break;
+            }
+        }
+        
+        return result;
+    }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
         
@@ -11,7 +65,17 @@ public class Search2dMatrix {
         int rows = matrix.length;
         int cols = matrix[0].length;
         
-        for(int r=0;r<rows;r++)
+        int start_row = 0;
+        int end_row   = rows-1;
+        
+        start_row = find_lower_bound(matrix,target,0,rows-1);
+        end_row   = find_upper_bound(matrix,target,0,rows-1);
+        
+        System.out.println(start_row+" "+end_row);
+        
+        if((start_row == -1) || (end_row ==-1)) return false;
+        
+        for(int r=start_row;r<=end_row;r++)
         {
             boolean is_candidate_row = (target>=matrix[r][0]) && (target<=matrix[r][cols-1]);
             
@@ -26,13 +90,11 @@ public class Search2dMatrix {
     }
     public static void main(String[] args) {
         int matrix[][] = {
-                            {1,4,7,11,15},
-                            {2,5,8,12,19},
-                            {3,6,9,16,22},
-                            {10,13,14,17,24},
-                            {18,21,23,26,30}
+                            {1},
+                            {3},
+                            {5}
                          };
-        int target = 5;
+        int target = 1;
         
         System.out.println(searchMatrix(matrix,target));
     }
