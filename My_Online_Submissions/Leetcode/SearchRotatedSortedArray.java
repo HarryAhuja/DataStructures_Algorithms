63a04f0206ed808c2021c9a1420c230f809b1f67
harry.ahuja <harryahuja2@gmail.com>
2021-07-31 12:28:48 +0530

    Search in rotated sorted array using Single Traversal [Binary Search]

--- My_Online_Submissions/Leetcode/SearchRotatedSortedArray.java
+++ My_Online_Submissions/Leetcode/SearchRotatedSortedArray.java
@@ -1,7 +1,5 @@
 /*
- * Find index of smallest number(pivot)
- * Find target side
- * Run normal Binary Search
+ * Single traversal
  */
 
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
@@ -10,47 +8,41 @@ import datastructures.DataStructures_Algorithms.Library_Api.BinarySearch;
 
 public class SearchRotatedSortedArray {
     
-    public static int find_pivot_element(int inp[], int low,int high)
+    public static int find_index_rotated_array_helper(int inp[], int target, int low,int high)
     {
+        int result = -1;
+        
         while(low<=high)
         {
             int mid = low + (high-low)/2;
             
-            if((mid-1)>=low && (mid+1)<=high && 
-               inp[mid]<inp[mid-1] && inp[mid]<inp[mid+1])
+            if(inp[mid] == target)  return mid;
+            
+            //Left Half Sorted
+            if(inp[mid]>=inp[low])
             {
-                return mid;
-              
+                if(target>=inp[low] && target<inp[mid]) high = mid-1; // Target lies in left sorted
+                else                                    low  = mid+1; // Target lies in right unsorted
             }
-            else if(inp[mid]>inp[high])
-            {
-                low = mid+1;
-            }
+            // Right Half Sorted
             else
             {
-                high = mid-1;
-            }   
+                if(target>inp[mid] && target<=inp[high]) low  = mid+1; // Target lies in right sorted
+                else                                     high = mid-1; // Target lies in left unsorted
+            }
         }
-        return low;
+        
+        return result;
     }
     
     public static int find_index_rotated_array(int inp[], int target)
     {
-        BinarySearch bs = new BinarySearch();
-        
         int n = inp.length;
          
         if(n==0)    return -1;
         if(n==1)    return (inp[0]==target)?0:-1;
         
-        int smallest_pivot_element = find_pivot_element(inp,0,n-1);
-       
-        System.out.println(smallest_pivot_element);
-        
-        if(smallest_pivot_element>0 && target>=inp[0] && target<=inp[smallest_pivot_element-1])
-            return bs.search_element(inp, target, 0, smallest_pivot_element-1);
-        else
-            return bs.search_element(inp, target, smallest_pivot_element, n-1);
+        return find_index_rotated_array_helper(inp, target,0,n-1);
     }
 
     public static void main(String[] args) {
