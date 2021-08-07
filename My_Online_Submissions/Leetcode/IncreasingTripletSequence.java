48c5b87953790e64e2e7034796f085b2442c340e
harry.ahuja <harryahuja2@gmail.com>
2021-08-07 23:27:40 +0530

    Handling duplicates[Increasing Triplet Sequence]

--- My_Online_Submissions/Leetcode/IncreasingTripletSequence.java
+++ My_Online_Submissions/Leetcode/IncreasingTripletSequence.java
@@ -10,6 +10,9 @@ public class IncreasingTripletSequence {
         
         for(int i=0;i<nums.length;i++)
         {
+            // = is important
+            // If not set, then {1,1,1,2} will return true
+            // which is wrong answer
             if(nums[i]<=small)      small = nums[i];
             else if(nums[i]<=big)   big = nums[i];
             else                    return true;
@@ -19,8 +22,9 @@ public class IncreasingTripletSequence {
     }
     public static void main(String[] args) {
         
-        int inp[]= {2,1,5,0,4,6};
-
+        //int inp[]= {2,1,5,0,4,6};
+        int inp[]= {1,1,1,2};
+        
         System.out.println(increasingTriplet(inp));
 
     }
