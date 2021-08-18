2a5459c2224276447dbb63668ace34a26fbb6f2c
harry.ahuja <harryahuja2@gmail.com>
2021-08-18 18:59:35 +0530

    Merge Intervals using Array reference

--- My_Online_Submissions/Leetcode/MergeIntervals.java
+++ My_Online_Submissions/Leetcode/MergeIntervals.java
@@ -9,90 +9,52 @@
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-class NodeComparator implements Comparator<Node>
+class NodeComparator implements Comparator<int[]>
 {
-    public int compare(Node n1,Node n2)
+    public int compare(int[] n1,int[] n2)
     {
-        return n1.start-n2.start;
+        return n1[0]-n2[0];
     }
 
 }
 
-class Node
-{
-    int start;
-    int end;
-    
-    public Node()
-    {
-        start = 0;
-        end   = 0;
-    }
-    public Node(int s,int e)
-    {
-        start = s;
-        end   = e;
-    }
-}
 public class MergeIntervals {
 
     public static int[][] merge_interval(int inp[][])
     {
         int n = inp.length;
-        if(n==0)    return new int[1][2];
+        if(n<=1)    return inp;
         
-        List<Node> list = new ArrayList<>();
-        ArrayList<Node> sorted = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
+        Arrays.sort(inp,new NodeComparator());
         
-        for(int i=0;i<n;i++)
-        {
-            sorted.add(new Node(inp[i][0],inp[i][1]));
-        }
-        
-        Collections.sort(sorted,new NodeComparator());
        
-        list.add(new Node(sorted.get(0).start,sorted.get(0).end));
-        int i = 1;
+       
+        int prev_entry[] = inp[0];
+        result.add(prev_entry);
         
+        int i = 1;
         while(i<n)
-        {
-            int prev_start = list.get(list.size()-1).start;
-            int prev_end   = list.get(list.size()-1).end;
-            int curr_start = sorted.get(i).start;
-            int curr_end   = sorted.get(i).end;
-            
-            if(curr_end<=prev_end)
+        {            
+            if(inp[i][0]<=prev_entry[1])
             {
-                list.remove(list.size()-1);
-                list.add(new Node(prev_start,prev_end));
-                
-            }
-            else if(curr_start<=prev_end)
-            {
-                list.remove(list.size()-1);
-                list.add(new Node(prev_start,curr_end));
+                prev_entry[1] = Math.max(inp[i][1], prev_entry[1]);
             }
             else
             {
-                list.add(new Node(curr_start,curr_end));
+                prev_entry = inp[i];
+                result.add(prev_entry);
             }
-            
             i++;
         }
         
-        int result[][] = new int[list.size()][2];
-        
-        for(i=0;i<list.size();i++)
-        {
-            result[i][0] = list.get(i).start;
-            result[i][1] = list.get(i).end;
-        }
-        
-        return result;
+        // Convert List of 1d array to 2d array
+        return result.toArray(new int[result.size()][]);
     }
     public static void main(String[] args) {
         int inp[][] = {{1,4},{0,2},{3,5}};
