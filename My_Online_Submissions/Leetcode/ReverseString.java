7f788a0a17dd2ed91d51d1dbee7af2b995482d92
harry.ahuja <harryahuja2@gmail.com>
2021-08-25 20:46:18 +0530

    Reverse String using faster Iterative [String]

--- My_Online_Submissions/Leetcode/ReverseString.java
+++ My_Online_Submissions/Leetcode/ReverseString.java
@@ -2,25 +2,19 @@ package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 public class ReverseString {
 
-    public static int first = 0;
-    
-    public static void reverse_helper(char inp[],int last,int n)
-    {
-        if(last == n)   return;
-        
-        reverse_helper(inp, last+1,n);
-        
-        if(first>=last)         return;
-        
-        char  temp = inp[first];
-        inp[first] = inp[last];
-        inp[last]  = temp;
-        
-        first++;
-    }
     public static void reverse(char inp[])
     {
-        reverse_helper(inp,0,inp.length);
+        int i = 0;
+        int j = inp.length-1;
+        
+        while(i<j)
+        {
+            char temp = inp[i];
+            inp[i] = inp[j];
+            inp[j] = temp;
+            i++;
+            j--;
+        }
     }
     public static void main(String[] args) {
         char inp[] = {'h','e','l','l','o'};
