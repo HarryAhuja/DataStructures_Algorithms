411edfaef1496f32394909d72ee7c376f3159f09
harry.ahuja <harryahuja2@gmail.com>
2021-08-26 12:55:02 +0530

    Using Character Array [Valid Palindrome]

--- My_Online_Submissions/Leetcode/ValidPalindrome.java
+++ My_Online_Submissions/Leetcode/ValidPalindrome.java
@@ -1,14 +1,6 @@
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 public class ValidPalindrome {
-    public static boolean is_alphanumeric(String s)
-    {
-        char c = s.charAt(0);
-        
-        if((c>='A' && c<='Z') || (c>='a' && c<='z') || (c>='0' && c<='9'))
-            return true;
-        return false;
-    }
     
     public static boolean isPalindrome(String s)
     {
@@ -18,25 +10,19 @@ public class ValidPalindrome {
         
         int i=0,j=n-1;
         
+        char c[] = s.toCharArray();
+        
         while(i<j)
         {
-            String  ith_str = s.charAt(i)+"";
-            String  jth_str = s.charAt(j)+"";
-            
-            boolean low  = is_alphanumeric(ith_str);
-            boolean high = is_alphanumeric(jth_str);
-            
-            if(low == true && high==true)
+            if(Character.isLetterOrDigit(c[i])==false)        i++;
+            else if (Character.isLetterOrDigit(c[j])==false)  j--;
+            else
             {
-                if(ith_str.equalsIgnoreCase(jth_str))
-                {    
-                    i++;j--;
-                }
-                else    return false;
-            }
-            else if(low==false) i++;
-            else                j--;
-            
+                if(Character.toLowerCase(c[i])!=Character.toLowerCase(c[j]))
+                    return false;
+                i++;
+                j--;
+            } 
         }
         return true;
     }
