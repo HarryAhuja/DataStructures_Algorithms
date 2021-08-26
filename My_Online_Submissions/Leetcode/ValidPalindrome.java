7280528844e0d80862b0feacec54183f2d1be523
harry.ahuja <harryahuja2@gmail.com>
2021-08-26 12:01:32 +0530

    Valid Palindrom Case Insenstive Compare [String]

---
+++ My_Online_Submissions/Leetcode/ValidPalindrome.java
@@ -0,0 +1,46 @@
+package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
+
+public class ValidPalindrome {
+    public static boolean is_alphanumeric(char c)
+    {
+        if((c>='A' && c<='Z') || (c>='a' && c<='z') || (c>='0' && c<='9'))
+            return true;
+        return false;
+    }
+    public static boolean isPalindrome(String s)
+    {
+        int n = s.length();
+        
+        if(n==0 || n==1)    return true;
+        
+        int i=0,j=n-1;
+        
+        while(i<j)
+        {
+            char  ith_char = s.charAt(i);
+            char  jth_char = s.charAt(j);
+            boolean low    = is_alphanumeric(ith_char);
+            boolean high   = is_alphanumeric(jth_char);
+            
+            if((low == true) && (high == true))
+            {
+                int diff = Math.abs(ith_char-jth_char);
+                if(diff == 32 || diff == 0)
+                {
+                    i++;
+                    j--;
+                }
+                else    return false;
+            }
+            else if(low == false)   i++;
+            else                    j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        
+        String inp = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(inp));
+    }
+
+}
