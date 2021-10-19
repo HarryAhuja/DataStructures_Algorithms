c36bdb61524cfdc393d2d632674f3f9931b1fc14
harry.ahuja <harryahuja2@gmail.com>
2021-10-19 16:48:21 +0530

    Level order using df [BT]

--- My_Online_Submissions/Leetcode/LevelOrderTraversal.java
+++ My_Online_Submissions/Leetcode/LevelOrderTraversal.java
@@ -1,48 +1,46 @@
 /*
- * 
+ * Below dfs solution works bcs we visit in pre-order sequence
+ * We will add new list whenever we encounter very first node in a level
+ * bcs we are visiting left before right(level order), that means if left
+ * is null also, then we add new list at right bcs right is first node at
+ * that level
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 
 public class LevelOrderTraversal {
-    public void level_order_helper(TreeNode root,List<List<Integer>> l)
+    public static void level_order_helper(TreeNode root,List<List<Integer>> l, int height)
     {
         if(root == null)    return;
         
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        
-        queue.offer(root);
-        
-        while(queue.isEmpty()==false)
+        if(height>=l.size())
         {
-            int level_num = queue.size();
-            List<Integer> sub_list = new LinkedList<>();
-            
-            for(int i=1;i<=level_num;i++)
-            {
-                TreeNode front = queue.poll();
-                sub_list.add(front.val);
-                
-                // ArrayDeque doesn't allow null
-                if(front.left!=null)     queue.offer(front.left);
-                if(front.right!=null)    queue.offer(front.right);
-            }
-            
-            l.add(sub_list);
+            l.add(new LinkedList<Integer>());
         }
+        
+        l.get(height).add(root.val);
+        
+        level_order_helper(root.left,l,height+1);
+        level_order_helper(root.right,l,height+1);
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public static List<List<Integer>> levelOrder(TreeNode root) {
         
         List<List<Integer>> l = new LinkedList<List<Integer>>();
         
-        level_order_helper(root,l);
+        level_order_helper(root,l,0);
         
         return l;
+    }
+    public static void main(String arg[])
+    {
+        TreeNode root         = new TreeNode(3);
+        root.left             = new TreeNode(9);
+        root.right            = new TreeNode(10);
+        root.right.left       = new TreeNode(15);
+        root.right.right      = new TreeNode(7);
+        List<List<Integer>> l = levelOrder(root);
     }
 }
