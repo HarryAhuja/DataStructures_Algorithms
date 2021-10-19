b25400b31de601f5ae89b0052a143100e439a957
harry.ahuja <harryahuja2@gmail.com>
2021-10-19 23:43:29 +0530

    Palindrome List using Local head[Linked List]

--- My_Online_Submissions/Leetcode/PalindromicList.java
+++ My_Online_Submissions/Leetcode/PalindromicList.java
@@ -2,6 +2,24 @@
  * This solution is slow since it takes double iteration
  * We know it is palindrome when head reaches back to center
  * but we are still comparing. So double iteration
+ * 
+ * Stopping at halfway?
+ * 1.) find mid and start from mid
+ * 2.) We are going till end, If we stop at midway also, recursion
+ *     would definitely come back. So complexity remains same
+ * 3.) We can track of global variable which will become false 
+ *     when scenario hit and based on that we can return at start of func
+ * 4.) instead of tracking global, make always local variables.
+ *     Each function should be self contained
+ * 5.) Not keeping global pointer would introduce one challenge
+ *     head will not be incremented globally. So to reflect changes
+ *     in head, return node from function.
+ *     So either return two values(head+boolean) or keep boolean as
+ *     global
+ * 6.) This solution condition (is_palindrome==false) also doesn't reduce
+ *     time bcs even if the the flag becomes false, this line will not hit
+ *     Because we are coming back from recursion and this line is above
+ *     the call.
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
@@ -16,32 +34,28 @@ class LinkedListNode
 
 public class PalindromicList {
     
-    static LinkedListNode low;
+    public static boolean is_palindrome = true;
     
-    public static boolean is_palindrome_helper(LinkedListNode head)
+    public static LinkedListNode is_palindrome_helper(LinkedListNode head,LinkedListNode curr)
     {
-        if(head == null)    return true;
+        if(curr == null || (is_palindrome==false))    return head;
         
-        boolean sub_ans = is_palindrome_helper(head.next);
+        head = is_palindrome_helper(head,curr.next);
          
-        if(low.val != head.val) return false;
+        if(head.val != curr.val) is_palindrome = false;
         
-        low = low.next;
-        
-        return sub_ans;
+        return head.next;
     }
     public static boolean isPalindrome(LinkedListNode head) {
-        
-        low = head;
-        return is_palindrome_helper(head);
-        
+        head =  is_palindrome_helper(head,head);
+        return is_palindrome;
     }
     public static void main(String[] args) {
        
         LinkedListNode head = new LinkedListNode(1);
         head.next = new LinkedListNode(2);
-        head.next.next = new LinkedListNode(2);
-        head.next.next.next = new LinkedListNode(1);
+        head.next.next = new LinkedListNode(3);
+        head.next.next.next = new LinkedListNode(4);
         
         System.out.println(isPalindrome(head));
 
