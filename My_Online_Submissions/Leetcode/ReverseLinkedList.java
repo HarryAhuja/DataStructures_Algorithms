ef859eba9ba02b3806eb88106f66eb0040569b0b
harry.ahuja <harryahuja2@gmail.com>
2021-10-20 17:52:09 +0530

    Reverse Linked List using iterative approach

--- My_Online_Submissions/Leetcode/ReverseLinkedList.java
+++ My_Online_Submissions/Leetcode/ReverseLinkedList.java
@@ -12,29 +12,24 @@ public class ReverseLinkedList {
     }
     public static LinkedListNode reverseList(LinkedListNode head) {
         
-        // head.next is important bcs we dont want to do anything with last node
-        // its next pointer is changed from its previous node not at itself 
-        // iteration
-        if(head == null || head.next==null)    return head;
+        if(head == null)    return head;
         
-        LinkedListNode sub_ans = reverseList(head.next);
+        LinkedListNode curr = head, prev = null, next;
         
-        // don't use sub_ans.next = head bcs
-        // head should change on each iteration but sub_ans always remain
-        // same bcs at end also we have to return last node
-        head.next.next = head;
-        head.next = null;
-        
-        return sub_ans;
+        while(curr!=null)
+        {
+            next      = curr.next;
+            curr.next = prev;
+            prev      = curr;
+            curr      = next;
+        }
+        return prev;
     }
     
     public static void main(String[] args) {
        
         LinkedListNode head = new LinkedListNode(1);
-        head.next = new LinkedListNode(2);
-        head.next.next = new LinkedListNode(3);
-        head.next.next.next = new LinkedListNode(4);
-        head.next.next.next.next = new LinkedListNode(5);
+
         print_list(head);
         
         head = reverseList(head);
