c3b37f3b33f7b432f5f126a3e12597ec7980ceba
harry.ahuja <harryahuja2@gmail.com>
2021-10-24 21:49:07 +0530

    Uncompleted min Stack

--- My_Online_Submissions/Leetcode/MinStack.java
+++ My_Online_Submissions/Leetcode/MinStack.java
@@ -1,80 +1,122 @@
 /*
- * Space : O(2N)
- * 
- * 
- * |  d,a   |
- * |  c,a   |
- * |  b,a   |
- * |  a,a   |
- * |        |
- * 
- * Suppose a is min element among elements from a to d
- * So whenever getMin() is called within this range, a will be returned
- * if any other element is min above d, it will be updated
- * a will remain min among this range till a is popped at last
- * 
+ *  Space  = O(N)
+ *  
+ *   Proof of modified value<new_minimum
+ *   
+ *   new value is < prev_minimum, we are pushing 2*new_value-prev_minimum
+ *   
+ *   We know that at this moment
+ *   new_value<prev_minimum
+ *   new_value-prev_minimum<0
+ *   2*new_value-prev_minimum<new_value
+ *   modified_value<new_minimum
+ *   
+ *   --> Any pop/top gives you value that is less that present_minimum, that
+ *   means we have pushed modified value not original value
+ *   
+ *   How to rollback to prev_minimum if current_minimum is popped
+ *   prev_minimum = 2*curr_minimum-stack.pop
+ *   
+ *   Here stack.pop  = 2*new_val-prev_minimum
+ *   and new_value = current_minimum bcs we pushed modified when new_value<prev_minimum
+ *   
+ *   2*curr_minimum-stack.pop
+ *   2*curr_minimum-(2*curr_minimum-prev_minimum)
+ *   =prev_minimum
+ *   
+ *   If we want to check the top
+ *   if(top<minimum) that means its a modified value
+ *   so top will be minimum bcs we have pushed modified value when its <prev_minimum
+ *   so actually new minimum has to be pushed otherwise
+ *   
+ *   Because we are using 2*val, use long
+ *   also range is equal to Integer.max
+ *   
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 import java.util.Stack;
 
-class StackPair
-{
-    int value;
-    int min;
-    public StackPair() {value=-1;min=Integer.MAX_VALUE;}
-    public StackPair(int v,int m) {value=v;min=m;}
-}
-
 public class MinStack {
     
-    public static Stack<StackPair> stack;
+    public static Stack<Long> stack;
+    public static Long curr_minimum;
     
     public MinStack()
     {
-        stack = new Stack<>();
+        curr_minimum    = Integer.MAX_VALUE+1L;
+        stack           = new Stack<>();
     }
     
     public static void push(int val)
     {
         if(stack.isEmpty()==true)
         {
-            stack.push(new StackPair(val,val));
+            stack.push(val+0L);
+            curr_minimum = val+0L;
         }
         else
         {
-            int min = stack.peek().min;
-            if(val<=min)  stack.push(new StackPair(val,val));
-            else            stack.push(new StackPair(val,min));
+            if(val<curr_minimum)
+            {
+                stack.push(2*val-curr_minimum+0L);
+                curr_minimum = val+0L;
+            }
+            else    stack.push(val+0L);
         }
     }
     
     public static void pop()
     {
-        if(stack.isEmpty()!=true)  stack.pop();
+        if(stack.isEmpty()!=true)
+        {
+            Long top = stack.pop();
+            if(top<curr_minimum)
+            {
+                curr_minimum =(2*curr_minimum)-top;
+            }
+        }
     }
     
     public static int top()
     {
-        if(stack.isEmpty()!=true)  return stack.peek().value;
+        if(stack.isEmpty()!=true)
+        {
+            Long top = stack.peek();
+            if(top<curr_minimum)    return curr_minimum.intValue();
+            else                    return top.intValue();
+        }
         else                        return -1;
     }
     
-    public static int getMin()
+    public static Long getMin()
     {
-        if(stack.isEmpty()==true)   return -1;
-        else                        return stack.peek().min;
+        if(stack.isEmpty()==true)   return -1L;
+        else                        return curr_minimum;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+         
         MinStack stack = new MinStack();
-        push(-2);
-        push(0);
-        push(-3);
+        push(2147483646);
+        push(2147483646);
+        push(2147483647);
+        System.out.println(top());
+        pop();
         System.out.println(getMin());
         pop();
+        System.out.println(getMin());
+        pop();
+        push(2147483647);
         System.out.println(top());
         System.out.println(getMin());
-
+        push(-2147483648);
+        //
+        System.out.println(top());
+        System.out.println(getMin());
+        pop();
+        //
+        System.out.println(getMin());
     }
 
 }
