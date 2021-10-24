c8e6248d0e643db54d708134667362a7be5b5ada
harry.ahuja <harryahuja2@gmail.com>
2021-10-24 14:53:41 +0530

    Iterative inorder [Stack]

--- My_Online_Submissions/Leetcode/InorderBT.java
+++ My_Online_Submissions/Leetcode/InorderBT.java
@@ -1,15 +1,47 @@
+/*
+ * Think on similar lines of recursion
+ * 
+ *  if(root==null)
+ *  left
+ *  print
+ *  right
+ *  
+ *  Once left becomes null, we pop root, print it and call right
+ * 
+ */
+
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InorderBT {
     public static void inorderTraversalHelper(TreeNode root,List<Integer> l) {
         if(root==null)  return;
         
-        inorderTraversalHelper(root.left,l);
-        l.add(root.val);
-        inorderTraversalHelper(root.right,l);
+        Stack<TreeNode> stack = new Stack<>();
+        
+        TreeNode node = root;
+       
+        while(true)
+        {
+            if(node!=null)
+            {
+                stack.push(node);
+                node = node.left;
+            }
+            else
+            {
+                // Before pop
+                if(stack.isEmpty()==true)   break;
+                
+                node = stack.pop();
+                System.out.println(node.val+" ");
+                
+                node = node.right;
+            }
+        }
     }
     public static List<Integer> inorderTraversal(TreeNode root) {
         
