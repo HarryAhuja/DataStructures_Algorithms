6215956f386d0ab6fc1dadb27b90220574cf06b9
harry.ahuja <harryahuja2@gmail.com>
2021-10-26 02:02:50 +0530

    Move zeroes comments

--- My_Online_Submissions/Leetcode/MoveZeroes.java
+++ My_Online_Submissions/Leetcode/MoveZeroes.java
@@ -1,3 +1,17 @@
+/*
+ * Filling zeroes vs swapping
+ * 
+ * 2.) 1,0,2,13
+ *     0 is swapped with 2
+ *     1,2,0,13
+ *     0 is swapped with 13
+ *     
+ *     So firstly 0 is swapped and then again some other number replaced it
+ *     we are unnecessary swapping first
+ *     
+ * 1.) First approach doesn't do this. it writes only single time 0 at last
+ * 
+ */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 public class MoveZeroes {
