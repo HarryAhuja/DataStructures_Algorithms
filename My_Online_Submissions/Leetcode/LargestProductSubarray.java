bc3f7ace823ecfbb62aee9c04e0b3760b7766ceb
harry.ahuja <harryahuja2@gmail.com>
2021-11-08 23:41:58 +0530

    Largest product subarray [Brute force]

--- My_Online_Submissions/Leetcode/LargestProductSubarray.java
+++ My_Online_Submissions/Leetcode/LargestProductSubarray.java
@@ -1,8 +1,33 @@
 /*
- * TC
- * 1.) [-2]-> single element
- * 2.) [0,2] -> For 2 it is not going into second loop and not being compared
+ * When all no are positive, then product will be always increasing
  * 
+ * When consecutive negative numebers, then sign will be alternative
+ * 
+ * DP
+ * [-1,-2,-3]
+ * Get answer of sub problem of -1,-2
+ * and then use -3 on top of that
+ * 
+ * max of subarray = 2
+ * min of subarray = -2
+ * 
+ * We can't skip -3 bcs of its subarray(consecutive)
+ * so taking -3 with max = -6
+ * taking    -3 with min = 6
+ * 
+ * When negative numeber is there, we required min element also bcs more is the 
+ * negative number, smaller the value is. So take min(more magnitude with negative sign)
+ * 
+ * If we encounter 0, then taking 0 in subarray means product is 0
+ * -> we can start a fresh start from next element
+ * 
+ * TC 
+ * {-1,8}
+ * max = -1
+ * min = -1
+ * 8*max , 8*min are less than 8 itself.
+ * So it should be
+ * max(current,current*max,current*min)
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
