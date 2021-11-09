5bde470451d611cb6826c318b1dbc971723a53c4
harry.ahuja <harryahuja2@gmail.com>
2021-11-09 22:36:57 +0530

    Ladder using Sliding window

--- My_Online_Submissions/Leetcode/LadderDP.java
+++ My_Online_Submissions/Leetcode/LadderDP.java
@@ -1,12 +1,16 @@
+/*
+ * Intitally for each n, we are iterating k backwards.So
+ * time was O(kn)
+ * 
+ * But with sliding window, we don't move k backwards always
+ * only subtract last and add current->O(1)
+ * So time is O(n)
+ * 
+ */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 public class LadderDP
 {
-    static int dp[] = new int[100000];
-    public static void init()
-    {
-        for(int i=0;i<100000;i++)   dp[i] = -1;
-    }
     
     public static int no_of_ways(int n,int k)
     {
@@ -14,23 +18,33 @@ public class LadderDP
         if(n<0)     return 0;
         if(n==0)    return 1;
         
-        if(dp[n] !=-1)  return dp[n];
+        int dp[] = new int[100000];
         
-        int ans = 0;
-        for(int i=1;i<=k;i++)
+        dp[0]   = 1;
+        int sum = dp[0];
+        int i   = 1;
+        for(i=1;i<n && i<k;i++)
         {
-            ans += no_of_ways(n-i,k);
+            dp[i] = sum;
+            sum+=dp[i];
         }
         
-        dp[n]  = ans;
+        // if(n<=k)
+        if(i==n)    return dp[n];
         
-        return ans;
+        for(;i<=n;i++)
+        {
+            dp[i] = sum;
+            // dp[i-1]+dp[i-2]+dp[i-3]+dp[i]-dp[i-3]
+            sum   = sum+dp[i]-dp[i-k];
+        }
+        
+        return dp[n];
     }
     public static void main(String[] args) {
        
         int n = 4, k = 3;
-        
-        init();
+       
         System.out.println(no_of_ways(n,k));
 
     }
