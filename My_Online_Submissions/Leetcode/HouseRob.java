4faf6b862d73ea17c2ddefb45c877954f52beea5
harry.ahuja <harryahuja2@gmail.com>
2021-11-10 22:51:23 +0530

    House Robbers using top down

--- My_Online_Submissions/Leetcode/HouseRob.java
+++ My_Online_Submissions/Leetcode/HouseRob.java
@@ -13,6 +13,13 @@ package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
 public class HouseRob {
 
+    static int dp[] = new int[101];
+    
+    public static void init()
+    {
+        for(int i=0;i<101;i++) dp[i] = -1;
+    }
+    
     public static int max(int a,int b)
     {
         return a>b?a:b;
@@ -20,6 +27,8 @@ public class HouseRob {
     public static int rob_helper(int nums[],int n)
     {
         if(n<=0)    return 0;
+        
+        if(dp[n]!=-1)    return dp[n];
         
         int inc = 0, exc = 0;
         
@@ -35,10 +44,13 @@ public class HouseRob {
         inc = nums[n-1]+rob_helper(nums,n-2);
         exc = rob_helper(nums,n-1);
         
-        return max(inc,exc);
+        
+        dp[n]=max(inc,exc);
+        return dp[n];
     }
     public static int rob(int[] nums)
     {
+        init();
         return rob_helper(nums,nums.length);
     }
     public static void main(String[] args) {
