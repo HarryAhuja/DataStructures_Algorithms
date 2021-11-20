159761128f6b81195e1ed6e9c7f683664becc0c1
harry.ahuja <harryahuja2@gmail.com>
2021-11-20 21:58:05 +0530

    Bst iterator comments

--- My_Online_Submissions/Leetcode/BSTIterator.java
+++ My_Online_Submissions/Leetcode/BSTIterator.java
@@ -3,6 +3,20 @@
  * 
  * Only push all left children
  * Push its right whenever call to next is made
+ * 
+ * The average time complexity of next() function is O(1) indeed in your case.
+ *  As the next function can be called n times at most, and the number of right nodes in 
+ *  self.pushAll(tmpNode.right) function is maximal n in a tree which has n nodes, 
+ *  so the amortized time complexity is O(1).
+ * 
+ * suppose we have total 10 nodes in tree
+ * in 2 calls of right, I added 5 and 5 nodes in stack
+ * 
+ * then rest of nodes will have right=null
+ * 
+ * in worst case next can be called 10 times
+ * so avg complexity=O(1)
+ * 
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
