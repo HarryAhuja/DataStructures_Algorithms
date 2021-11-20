6735ad225f97caf7d5180a7499f48e0aa133810e
harry.ahuja <harryahuja2@gmail.com>
2021-11-20 14:29:15 +0530

    Is valid bst with recursion different method

--- My_Online_Submissions/Leetcode/KthSmallestElementBst.java
+++ My_Online_Submissions/Leetcode/KthSmallestElementBst.java
@@ -17,42 +17,40 @@ package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
  }
 
 public class KthSmallestElementBst
-{
-    static int index = 0;
-    static int ans   = -1;
+{ 
+    // in online platform, dont make it static
+    // it will not get reset on multiple tc
+    public static int index = 0;
     
-    public static void kth_smallest_helper(TreeNode root, int k)
-    {
-        if(root == null)    return;
-        
-        kth_smallest_helper(root.left, k);
-        
-        index++;
-        if(index == k)
-        {
-            ans = root.val;
-            return;
-        }
-        
-        kth_smallest_helper(root.right, k);
-    }
+    
     public static int kthSmallest(TreeNode root, int k)
     {
         if(root == null)    return -1;
         
-        kth_smallest_helper(root,k);
+        int left_ans = kthSmallest(root.left, k);
         
-        return ans;
+        if(left_ans!=-1)   return left_ans;
+        
+        index++;
+        if(index == k)
+        {
+            return root.val;
+        }
+        
+        int right_ans = kthSmallest(root.right, k);
+        if(right_ans!=-1)   return right_ans;
+        
+        
+        return -1;
     }
     public static void main(String[] args) {
         TreeNode root = null;
         
-        root           = new TreeNode(3,null,null);
-        root.left      = new TreeNode(1,null,null);
-        root.right     = new TreeNode(4,null,null);
-        root.left.right = new TreeNode(2,null,null);
+        root           = new TreeNode(1,null,null);
+        root.right     = new TreeNode(2,null,null);
         
-        System.out.println(kthSmallest(root,1));
+        
+        System.out.println(kthSmallest(root,2));
 
     }
 
