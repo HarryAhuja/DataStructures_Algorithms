66cb5fa99dd76ebe485de271a436bea3bbd4bb80
harry.ahuja <harryahuja2@gmail.com>
2021-11-20 18:15:40 +0530

    Bst from preorder easy method

--- My_Online_Submissions/Leetcode/BstFromPreorder.java
+++ My_Online_Submissions/Leetcode/BstFromPreorder.java
@@ -13,42 +13,18 @@ public class BstFromPreorder {
         print_tree(root.right);
     }
 
-    public static void bstFromPreorderHelper(int[] preorder,int n,TreeNode current, 
-                                          int min,int max)
+    public static TreeNode bstFromPreorderHelper(int[] preorder,int n, int min,int max)
     {
-        if(index>=n)    return;
+        
+        if(index>=n || preorder[index]<min || preorder[index]>max)    return null;
         
         int value = preorder[index];
-        if(value>=min && value<=max && value<=(current.val))
-        {
-            current.left = new TreeNode(value);
-            index++;
-            bstFromPreorderHelper(preorder,n,current.left,min,current.val-1);
-        }
+        TreeNode root = new TreeNode(value);
         
-        /*
-         * Again checking and updating value is performed bcs above call to
-         * left in chaining can cause index to go out of bounds and value
-         * to be changed
-         * 
-         * Suppose at root there was some min and max and value lies in that range
-         * So we put that value to left child
-         * not index++ and value changed. Now it doesn't come in that range
-         * bcs value is to be placed in another child of some another root
-         * Therefore again check if new value is in range or not
-         * if not then backtrack
-         * 
-         */
+        root.left  = bstFromPreorderHelper(preorder,n,min,value-1);
+        root.right = bstFromPreorderHelper(preorder,n,value+1,max);
         
-        if(index>=n)    return;
-        value = preorder[index];
-        
-        if(value>=min && value<=max && value>=(current.val))
-        {
-            current.right = new TreeNode(value);
-            index++;
-            bstFromPreorderHelper(preorder,n,current.right,current.val+1,max);
-        }
+        return root;
     }
     public static TreeNode bstFromPreorder(int[] preorder)
     {
@@ -58,13 +34,9 @@ public class BstFromPreorder {
         int n = preorder.length;
         
         if(n==0)    return null;
-        
-        TreeNode root = new TreeNode(preorder[index]);
-        index++;
-        
-        bstFromPreorderHelper(preorder,n,root,min,max);
-        
-        return root;
+       
+        return bstFromPreorderHelper(preorder,n, min,max);
+
     }
     
     public static void main(String []a)
