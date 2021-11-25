420f17bc628e48049764c934a4f6f9058c9e1586
harry.ahuja <harryahuja2@gmail.com>
2021-11-26 02:05:10 +0530

    My_Online_Submissions/Leetcode/LongestSubstringWithoutRepeat imroved Time

--- My_Online_Submissions/Leetcode/LongestSubstringWithoutRepeat.java
+++ My_Online_Submissions/Leetcode/LongestSubstringWithoutRepeat.java
@@ -11,6 +11,39 @@
  *  
  *  But just jumping start pointer will not be enough
  *  All chars till last seen b should also be removed from hashmap
+ *  
+ *  Alternatively
+ *  abcdbapq
+ *  
+ *  when i am at second b
+ *  dont remove "a" from hashmap, only update start.We will get out max right
+ *  cdb
+ *  now i am at second a-> I can include in this substring since my start=2 index
+ *  but hashmap tells me that a is already there-> wrong info
+ *  How could I differentiate these cases
+ *  index of a last seen=0 < start
+ *  start would remain same
+ *  
+ *  vbadba
+ *  
+ *  now start=2
+ *  now at second a, last a seen=2
+ *  so if its ==start, then start would become last seen+1
+ *  
+ *  vbpadba
+ *  start=2
+ *  last seen a = 3
+ *  now its >start
+ *  now start = last seen+ 1
+ *  
+ *  so from all these cases
+ *  if(last_seen>=start)
+ *  start = last_seen+1
+ *  else
+ *  start = start
+ *  
+ *  or 
+ *  max(start,last_seen+1)
  * 
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
