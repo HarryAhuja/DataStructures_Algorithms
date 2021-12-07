d154ca2561b69b6137e2843ef49e6db2993215aa
harry.ahuja <harryahuja2@gmail.com>
2021-12-07 12:39:55 +0530

    Integer to roman without hashmaps

--- My_Online_Submissions/Leetcode/IntegerToRoman.java
+++ My_Online_Submissions/Leetcode/IntegerToRoman.java
@@ -26,65 +26,39 @@ import java.util.Set;
 
 public class IntegerToRoman
 {
-    // Intialising hashmap in literal way
-    // Map<String,Integer> map = Map.ofEntrie(Map.entry("a",1), Map.entry("b",2));
-    // Linked HashMap to preserve the order bcs i wanted to use 1000 first and then 900
-    // descending order-> above reason
-    static LinkedHashMap<String,Integer> map = new LinkedHashMap<String, Integer>();
-    boolean alreadyExecuted = false;
+    static String key_db[]  = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    static Integer val_db[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     
-    public void init_map()
+    public static String intToRoman(int num)
     {
-        map.put("M",1000);
-        map.put("CM",900);
-        map.put("D",500);
-        map.put("CD",400);
-        map.put("C",100);
-        map.put("XC",90);
-        map.put("L",50);
-        map.put("XL",40);
-        map.put("X",10);
-        map.put("IX",9);
-        map.put("V",5);
-        map.put("IV",4);
-        map.put("I",1);
-    }
-    public String intToRoman(int num)
-    {
-        if(alreadyExecuted==false)
-        {
-            init_map();
-            alreadyExecuted =  true;
-        }
-        
-        
         StringBuilder sb = new StringBuilder();
-        Set<Map.Entry<String, Integer>> entry_set = map.entrySet();
-        Iterator<Map.Entry<String, Integer>> itr  = entry_set.iterator();
-        
+        int i = 0;
         while(num>0)
         {
             // dont' reset itr back to 1000 bcs 
             // number is decreased now, we dont have to start again
             
-            while(itr.hasNext())
+            while(i<key_db.length)
             {
-                Map.Entry<String, Integer> e = itr.next();
-                String  key = e.getKey();
-                Integer val = e.getValue();
-                
-                
+                int val = val_db[i];
+            
                 if(num>=val)
                 {
                     int count = num/val;
-                    for(int i=0;i<count;i++)    sb.append(key);
+                    for(int j=0;j<count;j++)    sb.append(key_db[i]);
                     
                     num = num%val;
                 }
+                i++;
             }
             
         }
         
         return new String(sb);
+    }
+    
+    public static void main(String[] a)
+    {
+        System.out.println(intToRoman(400));
     }
 }
