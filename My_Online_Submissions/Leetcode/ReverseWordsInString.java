de4e05aed65427b7089d2985c92561e3951230d4
harry.ahuja <harryahuja2@gmail.com>
2021-12-07 11:34:27 +0530

    Median Finder comments and Reverse words in string using iterative method

--- My_Online_Submissions/Leetcode/ReverseWordsInString.java
+++ My_Online_Submissions/Leetcode/ReverseWordsInString.java
@@ -1,5 +1,10 @@
 /*
- * It worked but very slow since substring and recursion are used
+ * TC
+ * "  hello world  " -> bcs hello has leading spaces, it will hit inside while loop
+ *                      and append " " at the end 
+ * "the sky is blue"-> bcs the don't have leading spaces result.append(sb) will hit and
+ *                     will not append " " at the end
+ *  Aim : don't append space after last word in both cases;
  */
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
 
@@ -10,31 +15,46 @@ public class ReverseWordsInString {
         if( s== null || s.length()==0)  return "";
         
         int n =s.length();
+        StringBuilder result = new StringBuilder();
         StringBuilder sb = new StringBuilder();
         
+        int i=n-1;
         
-        
-        int i=0;
-        // skipping leading spaces
-        while(i<n && s.charAt(i) == ' ')
+        while(i>=0)
         {
-            i++;
+            char c = s.charAt(i);
+            
+            if(c==' ')
+            {
+                // if sb has some word
+                if(sb.length()!=0)
+                {
+                    result.append(sb+" ");
+                    
+                    // clear sb for next word
+                    sb.setLength(0);
+                }  
+            }
+            else
+            {
+                // if space is not seen, then insert c at first position
+                // appending c at last will make words reverse since we are 
+                // traversing from right to left
+                sb.insert(0, c);
+            }
+            i--;
         }
-        // building word
-        while(i<n && s.charAt(i) != ' ')
-        {
-            sb.append(s.charAt(i)+"");
-            i++;
-        }
-        String sub_ans = reverseWords(s.substring(i,n));
         
-        if(sub_ans.equals("")==false)   
-            return new String(sub_ans + " "+sb);
-        else
-            return new String(sb);
+        // append last sb when it comes out of while loop
+        result.append(sb);
+        
+        // to handle above discussed problem
+        if(result.charAt(result.length()-1)==' ')   result.setLength(result.length()-1);
+        
+        return new String(result);
     }
     public static void main(String[] args) {
-        System.out.println(reverseWords("  the sky is blue"));
+        System.out.println(reverseWords("  hello world  "));
 
     }
 
