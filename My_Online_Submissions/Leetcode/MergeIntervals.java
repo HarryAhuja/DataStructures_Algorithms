b0b5791edd6fe8215188e445b75e17fdbe97cb12
harry.ahuja <harryahuja2@gmail.com>
2021-12-08 11:09:28 +0530

    Overlapping subintervals using in place

--- My_Online_Submissions/Leetcode/MergeIntervals.java
+++ My_Online_Submissions/Leetcode/MergeIntervals.java
@@ -2,8 +2,20 @@
  * Challenges
  * 1.) inp will not be sorted acc to start point
  * 2.) [[1,4],[2,3]] full interval is within another
- * 3.) Use updated list no previous sorted list 
+ * 3.) Use updated list not previous sorted list 
  *     [[1,4],[0,2],[3,5]]
+ *     
+ *  after sorting starting point, these can be scenarios
+ *  
+ *  ----------      ---------       -> equal start-> end would be max of both
+ *  ------          ------------    -> in overlapped interval
+ *  
+ *  
+ *  ----------      ---------       -> unequal but overlapped with end-> end would be max of both
+ *    ------          ------------  -> in overlapped interval
+ *  
+ *  ------              -> not overlapped
+ *          -------
  */
 
 package datastructures.DataStructures_Algorithms.My_Online_Submissions.Leetcode;
@@ -18,6 +30,7 @@ class NodeComparator implements Comparator<int[]>
 {
     public int compare(int[] n1,int[] n2)
     {
+        // sort acc to starting point
         return n1[0]-n2[0];
     }
 
@@ -34,21 +47,31 @@ public class MergeIntervals {
         Arrays.sort(inp,new NodeComparator());
         
        
-       
+        // this is new overlapped interval
         int prev_entry[] = inp[0];
+        
+        // we are adding array reference to list
+        // that means we can change the element inside the list after adding also
         result.add(prev_entry);
+        // first entry is added as such in the list
+        // if any overlapping interval, it will be updated in the list
         
         int i = 1;
         while(i<n)
         {            
+            // if current starting point <= prev end point
             if(inp[i][0]<=prev_entry[1])
             {
+                // element inside the list is updated
                 prev_entry[1] = Math.max(inp[i][1], prev_entry[1]);
             }
             else
             {
                 prev_entry = inp[i];
                 result.add(prev_entry);
+                // add new entry in list
+                // if any overlapping interval with this, update inside the list
+                // if not remain as it is the list
             }
             i++;
         }
@@ -60,6 +83,13 @@ public class MergeIntervals {
         int inp[][] = {{1,4},{0,2},{3,5}};
         
         int result[][] = merge_interval(inp);
+        for(int i=0;i<result.length;i++)
+        {
+            for(int j=0;j<result[0].length;j++)
+                System.out.print(result[i][j]+" ");
+            System.out.println();
+        }
+        
 
     }
 
