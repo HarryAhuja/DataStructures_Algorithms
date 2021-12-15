01ba855480fcc16c9e534af620917bb9dd7f2095
harry.ahuja <harryahuja2@gmail.com>
2021-12-15 12:26:30 +0530

    Search 2d matrix different version using binary search

--- My_Online_Submissions/Leetcode/Search2dMatrix.java
+++ My_Online_Submissions/Leetcode/Search2dMatrix.java
@@ -9,20 +9,52 @@ public class Search2dMatrix {
     
     public int searchMatrix(ArrayList<ArrayList<Integer>> A, int B)
     {
+        
+        
         int n    = A.size();
         int m    = A.get(0).size();
-        int low  = 0;
-        int high = (n*m)-1;
         
+        if(n==0 && m==0)    return 0;           // corner TC
+        
+        int low  = 0;
+        int high = n-1;
+        int row  = -1;
+        
+        // Find upper bound of row
         while(low<=high)
         {
             int mid  = low+(high-low)/2;
             
-            if(B==A.get(mid/m).get(mid%m))
+            if(B==A.get(mid).get(0))
             {
                 return 1;
             }
-            else if(B>A.get(mid/m).get(mid%m))
+            else if(B>A.get(mid).get(0))
+            {
+                row = mid;
+                low = mid+1;
+                
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        
+        if(row==-1) return 0;   // no row       // corner TC
+        
+        // finding element in that particular row
+        low  = 0;
+        high = m-1;
+        while(low<=high)
+        {
+            int mid  = low+(high-low)/2;
+            
+            if(B==A.get(row).get(mid))
+            {
+                return 1;
+            }
+            else if(B>A.get(row).get(mid))
             {
                 low = mid+1;
             }
@@ -32,6 +64,7 @@ public class Search2dMatrix {
             }
         }
         return 0;
+        
     }
     
 }
